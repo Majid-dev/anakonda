c8629446576fd8cb25dfe19a10c130b871c7771f
Majid-dev <majid0karimi@gmail.com>
2023-04-12 16:52:30 +0430

    add apiv1 task resource

--- anakonda/resource/apiv1/__init__.py
+++ anakonda/resource/apiv1/__init__.py
@@ -1,2 +1,12 @@
 from anakonda.anakonda import apiv1 as api
+
 from .task import TaskResource
+
+api.add_resource(TaskResource, "/tasks", methods=["GET", "POST"], endpoint="tasks")
+
+api.add_resource(
+    TaskResource,
+    "/tasks/<string:task_id>",
+    methods=["GET", "PATCH", "DELETE"],
+    endpoint="task",
+)
---
+++ anakonda/resource/apiv1/task.py
@@ -0,0 +1,36 @@
+from flask_restful import Resource
+
+from anakonda.controller.apiv1.task import TaskController
+
+
+class TaskResource(Resource):
+    def get(self, task_id=None):
+        """
+        GET /api/v1/tasks --> List of tasks.
+        GET /api/v1/tasks/<task_id> --> Get task info.
+        """
+        if task_id is None:
+            return TaskController.get_tasks()
+        else:
+            return TaskController.get_task(task_id)
+
+    def post(self):
+        """
+        POST /api/v1/tasks --> Create new task.
+        Post /api/v1/tasks/<task_id> --> Not allowed.
+        """
+        return TaskController.create_task()
+
+    def patch(self, task_id):
+        """
+        PATCH /api/v1/tasks --> Not allowed.
+        PATCH /api/v1/tasks/<task_id> --> Update task.
+        """
+        return TaskController.update_task(task_id)
+
+    def delete(self, task_id):
+        """
+        DEL /api/v1/tasks --> Not allowed.
+        DEL /api/v1/tasks/<task_id> --> Update task.
+        """
+        return TaskController.delete_task(task_id)
