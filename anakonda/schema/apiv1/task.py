b3097dacce1f768fb189311db6336ba03af31562
Majid-dev <majid0karimi@gmail.com>
2023-04-23 14:59:18 +0430

    fix bug in TaskSchema class

--- anakonda/schema/apiv1/task.py
+++ anakonda/schema/apiv1/task.py
@@ -6,14 +6,13 @@ class TaskSchema(ma.SQLAlchemySchema):
     class Meta:
         model = Task
 
-
-id = ma.auto_field(dump_only=True)
-name = ma.auto_field()
-namespce = ma.auto_field()
-runtime = ma.auto_field()
-created_at = ma.auto_field(dump_only=True)
-last_update_at = ma.auto_field(dump_only=True)
-image = ma.auto_field()
-script = ma.auto_field()
-result = ma.auto_field(dump_only=True)
-status = ma.auto_field(dump_only=True)
+    id = ma.auto_field(dump_only=True)
+    name = ma.auto_field()
+    namespace = ma.auto_field()
+    runtime = ma.auto_field()
+    create_at = ma.auto_field(dump_only=True)
+    last_update_at = ma.auto_field(dump_only=True)
+    image = ma.auto_field()
+    script = ma.auto_field()
+    result = ma.auto_field(dump_only=True)
+    status = ma.auto_field(dump_only=True)
