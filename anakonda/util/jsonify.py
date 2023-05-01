e2342b8c5af00ccb58092f3690bdefd513ecf5b2
Majid-dev <majid0karimi@gmail.com>
2023-05-01 10:36:08 +0430

    update jsonify cods

--- anakonda/util/jsonify.py
+++ anakonda/util/jsonify.py
@@ -1,6 +1,17 @@
 from anakonda.config import Config
 
-STATUS_MESSAGE = {100: "OK", 101: "Method is not implemented"}
+STATUS_MESSAGE = {
+    100: "OK",
+    101: "Method is not implemented",
+    102: "Schema validation failed",
+    103: "Schema instance error",
+    104: "Invalid input provided",
+    105: "Media type is not supported",
+    106: "Database error",
+    107: "Resource not found",
+    108: "Runtime is not available",
+    109: "Resource is not updateable",
+}
 
 
 def jsonify(state={}, metadata={}, status=200, code=100, heasers={}):
